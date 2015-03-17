69d7f5740d25787af92189b0f5f176df96b95619
JakubKoziara <jakub.koziara1@gmail.com>
2015-03-17 13:42:16 +0000

    Destroy  Please enter the commit message for your changes. Lines starting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,6 +30,12 @@ end
     render 'edit'
   end
 end
+  def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+ 
+  redirect_to articles_path
+end
  
   private
   def article_params
