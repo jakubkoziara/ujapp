25adf0d4e3be18a7ccab56c49f751536bc240cb0
JakubKoziara <jakub.koziara1@gmail.com>
2015-03-31 12:04:12 +0000

    Autentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,19 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
+  
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
- def destroy
+  
+  def destroy
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
     redirect_to article_path(@article)
   end
+ 
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
